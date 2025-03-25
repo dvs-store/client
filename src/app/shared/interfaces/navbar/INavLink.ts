beeed6166cc63a99740c7c0aa81a53c38cdeb5d6
DevCompleteStudios <jorge.yael333@gmail.com>
2025-03-25 14:11:07 -0600

    mejorando navbar - faltan mejorar y reutilizar imagen de perfil

---
+++ src/app/shared/interfaces/navbar/INavLink.ts
@@ -0,0 +1,6 @@
+
+export interface INavLink {
+    path: string;
+    name: string;
+    icon: string;
+}
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,38 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { AuthService } from '../../auth/services/auth.service';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { INavLink } from '../interfaces/navbar/INavLink';
 
 @Component({
   selector: 'navbar',
-  imports: [],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
 
   protected authService = inject(AuthService);
+  protected links = signal<INavLink[]>([
+    {
+      name: 'Home',
+      path: '/',
+      icon: 'home',
+    },
+    {
+      name: 'Ranking',
+      path: '/ranking',
+      icon: 'ranking',
+    },
+    {
+      name: 'Messages',
+      path: '/messages',
+      icon: 'messages',
+    },
+    {
+      name: 'Support',
+      path: '/support',
+      icon: 'support',
+    },
+  ]);
 
 }
