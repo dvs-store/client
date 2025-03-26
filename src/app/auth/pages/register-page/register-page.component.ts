ff260248a974f4d9bc4cb3901c04f158ce249c21
DevCompleteStudios <jorge.yael333@gmail.com>
2025-03-26 08:15:56 -0600

    Agregando estilo a la creacion de una cuenta

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,33 +1,85 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, ChangeDetectionStrategy } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import {
   FormControl,
-  FormGroupDirective,
   Validators,
   FormsModule,
   ReactiveFormsModule,
   FormGroup,
 } from '@angular/forms';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {merge} from 'rxjs';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-register-page',
-  imports: [ReactiveFormsModule, MatInputModule, MatFormFieldModule],
-  templateUrl: './register-page.component.html'
+  imports: [ReactiveFormsModule, MatInputModule, MatFormFieldModule, FormsModule, MatButtonModule, MatIconModule],
+  templateUrl: './register-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class RegisterPageComponent {
 
-  private form = new FormGroup({
+  protected form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(70)]),
     name: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(80)]),
   });
-
+  protected formErrors = signal({
+    email: signal<string | null>(null),
+    password: signal<string | null>(null),
+    name: signal<string | null>(null),
+  })
   private authService = inject(AuthService);
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 
 
-  protected signUp(){}
+  constructor(){
+    merge(this.form.valueChanges, this.form.statusChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.handleErrors())
+  }
+
+
+  protected signUp(){
+    if(this.form.invalid) return;
+    console.log("Si paso");
+  }
+
+  private handleErrors(){
+    const controls = this.form.controls;
+    const email = controls.email;
+    const name = controls.email;
+    const password = controls.email;
+
+    if(email.hasError('email')){
+      this.formErrors().email.set('Email is not valid');
+    }else {
+      this.formErrors().email.set(null);
+    }
+
+    if(name.hasError('maxlength')){
+      this.formErrors().name.set('Name is too long');
+    }else if(name.hasError('minlength')){
+      this.formErrors().name.set('Name is too short');
+    }else {
+      this.formErrors().name.set(null);
+    }
+
+    if(password.hasError('maxlength')){
+      this.formErrors().password.set('Password is too long');
+    }else if(password.hasError('minlength')){
+      this.formErrors().password.set('Password is too short');
+    }else {
+      this.formErrors().password.set(null);
+    }
+  }
 
 }
