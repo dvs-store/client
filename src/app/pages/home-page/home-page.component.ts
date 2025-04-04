6d4a2e2d87671671d6432a4220fbe7dd72009621
DevCompleteStudios <jorge.yael333@gmail.com>
2025-04-03 20:24:36 -0600

    Agregando metatags en la pagina de inicio

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, contentChild, inject, OnInit } from '@angular/core';
 import { GetProductsComponent } from "../../store/products/components/get-products/get-products.component";
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home-page',
   imports: [GetProductsComponent],
   templateUrl: './home-page.component.html'
 })
-export default class HomePageComponent {
+export default class HomePageComponent implements OnInit {
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+
+  ngOnInit(): void {
+    this.addMetatags();
+  }
+
+
+  private addMetatags(){
+    const title:string = 'DevComplete Studios';
+    const description:string = 'Join us at DevComplete Studios and get the best Roblox services, Web Development, API Development, Machine Learning, everything related to Software Development.';
+
+    this.title.setTitle(title);
+    this.meta.updateTag({name: 'description', content: description});
+    this.meta.updateTag({name: 'og:description', content: description});
+    this.meta.updateTag({name: 'og:title', content: title});
+  }
 
 }
