1ffe900e669d04ad36c6767f2c363d4ff2b19b15
DevCompleteStudios <jorge.yael333@gmail.com>
2025-04-03 20:13:27 -0600

    Agregando metatags

--- src/app/store/products/pages/product-page/product-page.component.ts
+++ src/app/store/products/pages/product-page/product-page.component.ts
@@ -7,6 +7,8 @@ import { IProduct } from '../../interfaces/IProduct';
 import { CurrencyPipe, DatePipe, TitleCasePipe } from '@angular/common';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatExpansionModule} from '@angular/material/expansion';
+import { Meta, Title } from '@angular/platform-browser';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'product-page',
@@ -19,6 +21,8 @@ export default class ProductPageComponent implements OnInit {
   protected error = signal<string | null>(null);
   private productsService = inject(ProductsService);
   protected product = signal<IProduct | null>(null);
+  private title = inject(Title);
+  private meta = inject(Meta);
 
 
   ngOnInit(): void {
@@ -34,12 +38,27 @@ export default class ProductPageComponent implements OnInit {
     }
 
     this.productsService.findById(id)
+      .pipe(
+        finalize(() => this.addMetatags())
+      )
       .subscribe({
         error: (e) => this.error.set(HandleErrorsFn(e)),
         next: (product) => this.product.set(product),
       });
   }
 
-  private addMetatags(){}
+  private addMetatags(){
+    this.title.setTitle(`DVS | ${this.product()?.name ?? 'Products'}`);
+    const description = `Get the best ${this.product()?.categories.toString() ?? 'Software Enginer'} service with us, we provide you with security and the best quality`;
+
+    //Metatags
+    this.meta.updateTag({name: 'Description', content: description});
+    this.meta.updateTag({name: 'og:title', content: `DevComplete Studios | ${this.product()?.name}`});
+    this.meta.updateTag({name: 'og:description', content: description});
+
+    if( this.product() ){
+      this.meta.updateTag({name: 'og:image', content: `${this.product()!.image}`});
+    }
+  }
 
 }
